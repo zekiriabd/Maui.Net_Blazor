8f718114bd288bd42e961bc92939587f9912e1b9
zekiriabd@yahoo.fr <zekiriabd>
2022-09-11 18:37:32 +0200

    Add theme dark Lignt

---
+++ CustomComponent/CustomComponent/Models/ComponentM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomComponent.Models
+{
+    public class ComponentM
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Value { get; set; }
+        
+        public bool BoolValue { get { return Convert.ToBoolean(Value); } set { Value = value.ToString(); } }
+        
+        
+    }
+}
